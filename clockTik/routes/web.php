6e11c161ebe808c1e0f9df69fa57202c64220705
toondebooser <toondebooser@gmail.com>
2023-04-13 14:35:34 +0200

    Set up HomeController and route

--- clockTik/routes/web.php
+++ clockTik/routes/web.php
@@ -1,18 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'show'])->name('home');
+
+// Route::get('/new-user',  )
