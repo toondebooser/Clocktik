db480e44c7125bc7da73b4882310f6da34b44291
toondebooser <toondebooser@gmail.com>
2023-04-18 19:17:09 +0200

    Add logic for hour calculation and timesheet creator

--- clockTik/app/Http/Controllers/DashboardController.php
+++ clockTik/app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Timelog;
+use App\Models\TimeSheet;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -50,13 +51,47 @@ class DashboardController extends Controller
     public function stop()
     {
         $timeStamp = now();
+        $breakStatus = false;
         $userRow = Timelog::find(auth()->user()->id);
         if ($userRow->BreakStatus == true) {
+            $breakStatus = true;
             $userRow->BreakStatus = false;
         }
         $userRow->ShiftStatus = false;
         $userRow->StopWork = $timeStamp;
         $userRow->save();
+
+        $this->makeTimeSheet($userRow, $breakStatus, $timeStamp);
+
         return redirect('/dashboard');
+    }
+
+    public function makeTimeSheet($userRow, $breakStatus, $timeStamp)
+    {
+        $newTimeSheet = new TimeSheet;
+        $newTimeSheet->UserId = auth()->user()->id;
+        $newTimeSheet->ClockedIn = $userRow->StartWork;
+        $newTimeSheet->ClockedOut = $userRow->StopWork;
+        if ($breakStatus == true) {
+            $newTimeSheet->BreakStart = $userRow->StartBreak;
+            $newTimeSheet->BreakStop = $userRow->EndBreak;
+            $newTimeSheet->BreakHours = $this->calculateBreakHours($newTimeSheet, $userRow);
+        }
+        $newTimeSheet->RegularHours = $this->calculateRegularHours($newTimeSheet);
+        $newTimeSheet->OverTime = $this->calculateOverTime($newTimeSheet);
+        $newTimeSheet->Month = $timeStamp->format('F Y');
+        $newTimeSheet->save();
+    }
+
+    public function calculateBreakHours($newTimeSheet, $userRow)
+    {
+        $start = $userRow->StartBreak;
+        $end = $userRow->EndBreak;
+    }
+    public function calculateRegularHours($newTimeSheet)
+    {
+    }
+    public function calculateOverTime($newTimeSheet)
+    {
     }
 }
--- clockTik/resources/views/dashboard.blade.php
+++ clockTik/resources/views/dashboard.blade.php
@@ -11,21 +11,24 @@
         </a>
     @else
         @if ($shiftStatus == true && $breakStatus == false)
-            <a onclick="return confirm('Are you sure you want to take a break?')" href="{{ route('break') }}" class="breakButton">
+            <a onclick="return confirm('Are you sure you want to take a break?')" href="{{ route('break') }}"
+                class="breakButton">
                 <p class="buttonText">Break</p>
             </a>
-            <a onclick="return confirm('Are you sure you want to quit your shift?')" href="{{ route('stop') }}" class="stopButton">
+            <a onclick="return confirm('Are you sure you want to quit your shift?')" href="{{ route('stop') }}"
+                class="stopButton">
                 <p class="buttonText">Stop</p>
             </a>
         @else
             @if ($shiftStatus == true && $breakStatus == true)
-                <a onclick="return confirm('Are you sure you want to start working again?')" href="{{ route('stopBreak') }}" class="breakButton">
+                <a onclick="return confirm('Are you sure you want to start working again?')" href="{{ route('stopBreak') }}"
+                    class="breakButton">
                     <p class="buttonText">Back to work</p>
                 </a>
-                
-                <a onclick="return confirm('Are you sure you want to quit your shift?')" href="{{ route('stop') }}" 
+
+                <a onclick="return confirm('Are you sure you want to quit your shift?')" href="{{ route('stop') }}"
                     class="stopButton">
-                    <p  class="buttonText">Stop</p>
+                    <p class="buttonText">Stop</p>
                 </a>
             @else
                 <div class="text-danger">Something went wrong pls contact tech guy.</div>
