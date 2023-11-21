e576cc085ce2265d4faa514d75a084ee31d5918a
toondebooser <toondebooser@gmail.com>
2023-11-21 13:54:12 +0100

    Addapting app.js for production

--- clockTik/resources/views/layout.blade.php
+++ clockTik/resources/views/layout.blade.php
@@ -11,8 +11,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-     <!-- <link rel="stylesheet" href="{{ URL::asset('resources/css/app.css') }}">  -->
-     <!-- <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}"> -->
+    {{-- <link rel="stylesheet" href="{{ URL::asset('resources/css/app.css') }}">   --}}
+    {{-- <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}">  --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>clockTik</title>
 </head>
