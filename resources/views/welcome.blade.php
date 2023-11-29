6a36fa91730afc832b4fd1ca3fd8035c22fd9b0e
Toon <toondebooser@gmail.com>
2023-11-29 13:37:38 +0100

    Refine img tag syntax in the welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,7 @@
 @section('title')
     <h1>Clocktik</h1>
     <a href="{{ route('dashboard') }}" class="logoContainer">
-        <div class="logo">
-            <img src={{ asset('images/95090418_1090903184610004_7235939885578715136_n.png') }}
+            <img class="logo" src="{{ asset('images/95090418_1090903184610004_7235939885578715136_n.png') }}"
                 alt="hmm there should be a alogo here">
-        </div>
     </a>
 @endsection
