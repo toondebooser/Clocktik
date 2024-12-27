3b1456e0b81a069ff68e72fb3108ff6dd33989a7
toondebooser <toondebooser@gmail.com>
2024-12-27 18:43:19 +0100

    change version number on welcom blade

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -15,6 +15,9 @@
             border: 1px solid #dddddd;
             border-radius: 5px;
         }
+        .buttonText{
+            color: white
+        }
         .email-button {
             display: inline-block;
             margin-top: 20px;
@@ -31,7 +34,7 @@
         <h2>Verify Your Email Address</h2>
         <p>Hello,</p>
         <p>Please click the button below to verify your email address:</p>
-        <a href="{{ $url }}" class="email-button">Verify Email Address</a>
+        <a href="{{ $url }}" class="email-button"><p class="buttonText">Verify Email Address</p></a>
         <p>If you did not create this account, no further action is required.</p>
         <p>Thank you for using our application!</p>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,5 @@
             <img class="logo" src="{{ asset('images/95090418_1090903184610004_7235939885578715136_n.png') }}"
                 alt="hmm there should be a alogo here">
     </a>
-    <p style="grid-column: 1/13; justify-self:center; font-size:large">1.3.9</p>
+    <p style="grid-column: 1/13; justify-self:center; font-size:large">1.4.10</p>
 @endsection
