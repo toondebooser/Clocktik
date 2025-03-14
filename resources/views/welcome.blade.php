418fbca60d5d91098998e93b1ef1e7cb65d28480
toondebooser <toondebooser@gmail.com>
2025-03-14 17:21:03 +0100

    Fix condition for logo display in welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 @endif
     <h1>Tiktrack</h1>
     <a href="{{ route('dashboard') }}" class="logoContainer">
-            <img class="logo" src="{{ auth()->check() ? asset(auth()->user()->company->image) : asset('images/Taxus logo perfect round.png') }}"
+            <img class="logo" src="{{ auth()->check() && auth()->user()->company && auth()->user()->company->image ? asset(auth()->user()->company->image) : asset('images/Taxus logo perfect round.png') }}"
             alt="Company Logo">
     </a>
     <p style="grid-column: 1/13; justify-self:center; font-size:large">2.0.0</p>
