ff0e0f6e9d3e1d38a125a029ce472907690f4b5e
toondebooser <toondebooser@gmail.com>
2025-03-18 21:01:23 +0100

    Grok's refactored my-list.blade

--- resources/views/my-list.blade.php
+++ resources/views/my-list.blade.php
@@ -1,60 +1,59 @@
 @extends('layout')
+
 @section('content')
-@section('content')
+    <!-- Success message -->
     @if (session('success'))
         <div class="success">
             {{ session('success') }} <br>
-            <a class="removeError" href=""> ok </a>
+            <a class="removeError" href="#">OK</a> <!-- "#" voorkomt page reload -->
         </div>
     @endif
+
     <h2>{{ $type }}</h2>
 
     <div class="workersForm">
         @foreach ($dataSet as $data)
+            <!-- Personeel of Voor wie? -->
             @if ($type !== 'Bedrijven' && !$data->admin)
-                <form class='workerForm'
-                    action="{{ $type == 'Personeel' ? route('getData') : ($type == 'Voor wie?' ? route('specials') : '') }}"
-                    method="post">
+                <form class="workerForm" 
+                      action="{{ $type === 'Personeel' ? route('getData') : route('specials') }}" 
+                      method="post">
                     @csrf
-                    <button class='workerButton' type="submit" name='worker' value="{{ $data->id }}">
+                    <button class="workerButton" type="submit" name="worker" value="{{ $data->id }}">
                         {{ $data->name }}
-                        @switch(true)
-                            @case($data->timelogs->ShiftStatus == true && $data->timelogs->BreakStatus == false)
-                                <div class="working"></div>
-                            @break
-
-                            @case($data->timelogs->ShiftStatus == true && $data->timelogs->BreakStatus == true)
-                                <div class="onBreak"></div>
-                            @break
-
-                            @default
-                                <div class="notWorking"></div>
-                        @endswitch
+                        <div class="{{ $data->timelogs->ShiftStatus ? ($data->timelogs->BreakStatus ? 'onBreak' : 'working') : 'notWorking' }}"></div>
                     </button>
                 </form>
-            @elseif($type == "Bedrijven")
-                <form class='workerForm' action="{{ route('myList', ['type' => 'Personeel', 'company_code' => $data->company_code]) }}" method="get">
-                    @csrf
-                    <button style="display: flex; align-items: center; justify-content: center" class='workerButton' type="submit" name='worker'>
+
+            <!-- Bedrijven -->
+            @elseif ($type === 'Bedrijven')
+                <form class="workerForm" 
+                      action="{{ route('myList', ['type' => 'Personeel', 'company_code' => $data->company_code]) }}" 
+                      method="get">
+                    @csrf <!-- Niet nodig voor GET, kan weg -->
+                    <button class="workerButton" type="submit" name="worker" style="display: flex; align-items: center; justify-content: center;">
                         {{ $data->company_name }}
-                        <img style="height: 30px; margin-left: 10px" src="{{asset($data->image)}}" alt="Company logo">
+                        <img style="height: 30px; margin-left: 10px;" src="{{ asset($data->image) }}" alt="Company logo">
                     </button>
                 </form>
                 @if ($loop->last)
-                    <a class='specialsButton' href="{{ route('addCompany') }}">Bedrijf toevoegen</a>
+                    <a class="specialsButton" href="{{ route('addCompany') }}">Bedrijf toevoegen</a>
                 @endif
             @endif
         @endforeach
-        @if ($type == 'Voor wie?')
+
+        <!-- Voor iedereen knop -->
+        @if ($type === 'Voor wie?')
             <form class="workerForm" method="post" action="{{ route('specials') }}">
                 @csrf
-                <button class="workerButton" type="submit" name="worker" value="{{ $dataSet }}">
-                    Voor iedereen</button>
+                <button class="workerButton" type="submit" name="worker" value="{{ $dataSet }}">Voor iedereen</button>
             </form>
         @endif
     </div>
-    @if ($type == 'Personeel')
-        <a href="{{ route('myList', ['type' => 'Voorwie', 'company_code' => auth()->user()->company_code]) }}"
-            class='specialsButton'>Dagen instellen</a>
+
+    <!-- Dagen instellen link -->
+    @if ($type === 'Personeel')
+        <a href="{{ route('myList', ['type' => 'Voor wie?', 'company_code' => auth()->user()->company_code]) }}"
+           class="specialsButton">Dagen instellen</a>
     @endif
 @endsection
