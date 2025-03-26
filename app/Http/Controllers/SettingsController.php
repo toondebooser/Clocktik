d8ab7814085c0690155bb7422cc2791a6208ab8a
toondebooser <toondebooser@gmail.com>
2025-03-26 16:51:45 +0100

    search for company data and send it to settings blade

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
     public function settingsView ($company_code){
 
-        return view('settings', ['company_code', $company_code]);
+        $data = Company::where('company_code',$company_code)->first();
+
+        return view('settings', ['data' =>  $data]);
 
     }
+
+
 }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,3 +1,3 @@
 <div>
-    test
+   {{$data}}
 </div>
