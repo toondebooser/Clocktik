8c319f9b64cc92d2bf2956c0cc53d4b711eb25c9
toondebooser <toondebooser@gmail.com>
2025-04-09 18:28:36 +0200

    Add a side menu triggered by adding a classname through javascript

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;200;300;400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="icon" href="{{ auth()->check() && auth()->user()->company && auth()->user()->company->company_logo ? asset(auth()->user()->company->company_logo) : asset('images/TaxusLogo.png') }}" sizes="192x192" type="image/png">
+    <link rel="icon"
+        href="{{ auth()->check() && auth()->user()->company && auth()->user()->company->company_logo ? asset(auth()->user()->company->company_logo) : asset('images/TaxusLogo.png') }}"
+        sizes="192x192" type="image/png">
     @if (env('APP_ENV') == 'local')
         @vite('resources/css/app.css', 'resources/js/app.js')
     @elseif (env('APP_ENV') == 'production')
@@ -55,23 +57,33 @@
                 <a class="removeError" href="">ok</a>
             </div>
         @endif
+        @yield('header')
+        <div id="slide-menu" class="side-menu">
+        </div>
         <header>
-            @yield('header')
-            <a class="headerLinks" href="{{ route('home') }}">Home</a>
             @auth
-                @if ($currentUser->god)
+                <a class="headerLinks" href="{{ route('home') }}">Home</a>
+                @if ($currentUser->admin && $currentUser->company->admin_timeclock)
+                    <div class="headerLinks " onclick="toggle()">Hamburger</div>
+                @elseif ($currentUser->god)
                     <a class="authLinks button"
                         href="{{ route('myList', ['type' => 'Bedrijven', 'company_code' => $currentUser->company_code]) }}">Bedrijven</a>
-                @elseif (!$currentUser->admin || ($currentUser->admin && $currentUser->company->admin_timeclock))
+                @elseif (!$currentUser->admin)
                     <a class="headerLinks" href="{{ route('dashboard') }}">Timeclock</a>
-                    <a class="authLinks button" href="{{ route('myProfile') }}">Mijn profiel</a>
                     @if ($currentUser->admin && !$currentUser->god)
+                        <a href="{{ route('adminSettings', ['company_code' => $currentUser->company_code]) }}">
+                            <img style="height: 40px" src="{{ asset('images/settings.png') }}" alt="settings">
+                        </a>
                         <a class="authLinks button"
                             href="{{ route('myList', ['type' => 'Personeel', 'company_code' => $currentUser->company_code]) }}">Personeel</a>
                     @endif
-                {{-- @else
+                    <a class="authLinks button" href="{{ route('myProfile') }}">Mijn profiel</a>
+                @else
+                    <a href="{{ route('adminSettings', ['company_code' => $currentUser->company_code]) }}">
+                        <img style="height: 40px" src="{{ asset('images/settings.png') }}" alt="settings">
+                    </a>
                     <a class="authLinks button"
-                        href="{{ route('myList', ['type' => 'Personeel', 'company_code' => $currentUser->company_code]) }}">Personeel</a> --}}
+                        href="{{ route('myList', ['type' => 'Personeel', 'company_code' => $currentUser->company_code]) }}">Personeel</a>
                 @endif
                 <a class="authLinks button" href="{{ route('logout') }}">Logout</a>
             @endauth
@@ -86,7 +98,24 @@
 
         <footer> &copy Toon De Booser</footer>
     </div>
+    <script>
+        const toggle = () => {
+            const element = document.getElementById('slide-menu');
+            element.classList.toggle('slide-in');
+        };
+        window.toggle = toggle; 
 
+        document.addEventListener('click', (event) => {
+            const menu = document.getElementById('slide-menu');
+            const isMenuOpen = menu.classList.contains('slide-in');
+            const clickedInsideMenu = menu.contains(event.target);
+            const clickedToggle = event.target.closest('.headerLinks') && event.target.textContent === 'Hamburger';
+
+            if (isMenuOpen && !clickedInsideMenu && !clickedToggle) {
+                menu.classList.remove('slide-in');
+            }
+        });
+    </script>
 </body>
 
 </html>
