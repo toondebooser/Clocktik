8c3aef80605af8858e3e03a0a9a11560394dea0e
toondebooser <toondebooser@gmail.com>
2025-04-11 12:04:26 +0200

    Try and let ai rewrite the complex header conditions. But with no succes

--- resources/views/my-list.blade.php
+++ resources/views/my-list.blade.php
@@ -5,7 +5,7 @@
 
     <h2>{{ $type }}</h2>
     <div id="list" class="workersForm">
-        <input type="text" id="searchInput" placeholder="Zoek "> <br>
+        <input type="text" id="searchInput" placeholder="Zoek..."> <br>
         <div class="hidden" id="type" type="{{$type}}"></div>
         @foreach ($dataSet as $data)
             @if (($type !== 'Bedrijven' && !$data->admin) || ($data->admin && $data->company->admin_timeclock))
