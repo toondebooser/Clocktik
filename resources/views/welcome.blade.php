1bbe92eeda9dba6108cf580d5d8a29d60f3b8278
toondebooser <toondebooser@gmail.com>
2025-04-29 18:18:52 +0200

    Install spatie/hollidays package for future use

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,15 @@
 @extends('layout')
 @section('title')
- 
+
+{{-- @php
+    use Spatie\Holidays\Holidays;
+    use Spatie\Holidays\Countries\Belgium;
+    use Carbon\Carbon;
+
+
+    $holidays = Holidays::for('be')->getInRange(Carbon::now()->startOfMonth(),Carbon::now()->endOfmonth() );
+dd($holidays);
+@endphp --}}
     <h1>Tiktrack</h1>
     <a href="{{ route('dashboard') }}" class="logoContainer">
             <img class="logo" src="{{ auth()->check() && auth()->user()->company && auth()->user()->company->company_logo ? asset(auth()->user()->company->company_logo) : asset('images/TaxusLogo.png') }}"
